8190dbace7ca7728676f630685d41bed8a7e2cea
Elio Capella Sánchez <eliocapella@gmail.com>
2012-12-06 00:12:22 +0100

    improved lexer test helper

--- test/lexer.js
+++ test/lexer.js
@@ -4,32 +4,34 @@
 var vows = require("vows"),
     assert = require("assert");
 
-function lex(file) {
+function lex(file, expected) {
     var lexer = require("../source/lexer"),
         fs = require("fs"),
         path = require("path");
 
-    return function () {
-        var callback = this.callback;
-        fs.readFile(path.resolve("./test/code", file), "utf8",
-            function (err, code) {
-                callback(err, lexer.tokenize(code));
-            });
+    return {
+
+        topic: function () {
+            var callback = this.callback;
+            fs.readFile(path.resolve("./test/code", file), "utf8",
+                function (err, code) {
+                    callback(err, lexer.tokenize(code));
+                });
+        },
+
+        "Generates correct tokens": function (err, tokens) {
+            assert.ifError(err);
+            assert.deepEqual(tokens, expected);
+        }
+
     };
 }
 
 vows.describe("Lexer").addBatch({
 
-    "Hello world": {
-        topic: lex("hello-world.code"),
-
-        "generates correct tokens": function (err, tokens) {
-            assert.ifError(err);
-            assert.deepEqual(tokens, [["IDENTIFIER", "print", 1],
-                ["(", "(", 1], ["TEXT", "hello world", 1], [")", ")", 1],
-                ["EOF", "", 1]]);
-        }
-    }
+    "Hello world": lex("hello-world.code", [["IDENTIFIER", "print", 1],
+        ["(", "(", 1], ["TEXT", "hello world", 1], [")", ")", 1],
+        ["EOF", "", 1]])
 
 }).exportTo(module);
 
